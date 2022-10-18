292c7d6ecf8db96a99b94cfe6329b05149ab5f82
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-10-18 05:28:18 -0500

    added to vote-routes and refactored post.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Comment, Vote } = require('../../models');
 
-// GET /api/users: Will get all user in database
+
+// get all users
 router.get('/', (req, res) => {
-    // Access our User model and run .findAll() method)
   User.findAll({
     attributes: { exclude: ['password'] }
   })
@@ -14,28 +14,46 @@ router.get('/', (req, res) => {
     });
 });
 
-// GET /api/users/1: Will return one user based on req.params.id
 router.get('/:id', (req, res) => {
-
-    User.findOne({
-        where: {
-            id: req.params.id
+  User.findOne({
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ['id', 'title', 'post_url', 'created_at']
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'created_at'],
+        include: {
+          model: Post,
+          attributes: ['title']
         }
+      },
+      {
+        model: Post,
+        attributes: ['title'],
+        through: Vote,
+        as: 'voted_posts'
+      }
+    ]
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
     })
-        .then(dbUserData => {
-            if (!dbUserData) {
-                res.status(404).json({ message: 'No user found with this id' });
-                return;
-            }
-            res.json(dbUserData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-// POST /api/users: Will create a user
 router.post('/', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
@@ -58,7 +76,6 @@ router.post('/', (req, res) => {
     });
 });
 
-//Will retrieve a single users acnt
 router.post('/login', (req, res) => {
   User.findOne({
     where: {
@@ -88,11 +105,21 @@ router.post('/login', (req, res) => {
   });
 });
 
-// PUT /api/users/1: Updates existing data
-router.put('/:id', (req, res) => {
-      // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+});
 
-  // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+router.put('/:id', (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+
+  // pass in req.body instead to only update what's passed through
   User.update(req.body, {
     individualHooks: true,
     where: {
@@ -112,24 +139,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-// DELETE /api/users/1: Deletes a single user from the database
 router.delete('/:id', (req, res) => {
-    User.destroy({
-        where: {
-            id: req.params.id
-        }
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
     })
-        .then(dbUserData => {
-            if (!dbUserData) {
-                res.status(404).json({ message: 'No user found with this id' });
-                return;
-            }
-            res.json(dbUserData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
