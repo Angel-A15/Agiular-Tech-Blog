3bec1410a680f879bf20c95a49ce099c392b8617
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-10-17 21:26:12 -0500

    gitignore addon

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const session = require('express-session');
-const exhbs = require('express-handlebars');
+// const exhbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
 
-
-const routes = require('./routes');
+// const routes = require('./routes');
 const sequelize = require('./config/connection');
 
 
@@ -16,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
-app.use('./constrollers/');
+// app.use('./constrollers/');
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
